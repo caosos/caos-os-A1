d3c0c68b98d2fc34be5ac177d8f97fdbdfc2c56d
MICHAEL CHAMBERS <mytaxicloud@gmail.com>
2026-01-04 04:35:29 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,12 +1,14 @@
 import Chat from './pages/Chat';
 import Console from './pages/Console';
 import Welcome from './pages/Welcome';
+import Implementation from './pages/Implementation';
 
 
 export const PAGES = {
     "Chat": Chat,
     "Console": Console,
     "Welcome": Welcome,
+    "Implementation": Implementation,
 }
 
 export const pagesConfig = {
