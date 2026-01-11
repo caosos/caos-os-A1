ceb5a194f81f77b11fcb5c78b9ee72a7e9bb8682
MICHAEL CHAMBERS <mytaxicloud@gmail.com>
2026-01-11 00:39:04 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -2,6 +2,7 @@ import Chat from './pages/Chat';
 import Console from './pages/Console';
 import Implementation from './pages/Implementation';
 import Welcome from './pages/Welcome';
+import MemoryIsolation from './pages/MemoryIsolation';
 
 
 export const PAGES = {
@@ -9,6 +10,7 @@ export const PAGES = {
     "Console": Console,
     "Implementation": Implementation,
     "Welcome": Welcome,
+    "MemoryIsolation": MemoryIsolation,
 }
 
 export const pagesConfig = {
