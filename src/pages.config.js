c1fdbc961e3d7a114539705a4eed6653a4d080f9
MICHAEL CHAMBERS <mytaxicloud@gmail.com>
2026-01-25 03:48:09 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,32 +1,20 @@
 import Ballot from './pages/Ballot';
 import Chat from './pages/Chat';
 import Console from './pages/Console';
-import Home from './pages/Home';
 import Implementation from './pages/Implementation';
 import MemoryIsolation from './pages/MemoryIsolation';
-import Profile from './pages/Profile';
-import Search from './pages/Search';
 import TerminalBlueprint from './pages/TerminalBlueprint';
 import Welcome from './pages/Welcome';
-import MyProfile from './pages/MyProfile';
-import District from './pages/District';
-import Statistics from './pages/Statistics';
 
 
 export const PAGES = {
     "Ballot": Ballot,
     "Chat": Chat,
     "Console": Console,
-    "Home": Home,
     "Implementation": Implementation,
     "MemoryIsolation": MemoryIsolation,
-    "Profile": Profile,
-    "Search": Search,
     "TerminalBlueprint": TerminalBlueprint,
     "Welcome": Welcome,
-    "MyProfile": MyProfile,
-    "District": District,
-    "Statistics": Statistics,
 }
 
 export const pagesConfig = {
