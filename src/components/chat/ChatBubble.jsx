e18d7368e26cde3fa8f5d39708c180f100c7f90c
MICHAEL CHAMBERS <mytaxicloud@gmail.com>
2026-03-07 02:45:03 +0000

    File changes

--- src/components/chat/ChatBubble.jsx
+++ src/components/chat/ChatBubble.jsx
@@ -593,36 +593,7 @@ export default function ChatBubble({ message, isUser, onUpdateMessage, closeMenu
     return (
       <div className="space-y-3">
         {/* Embedded Videos */}
-        {videoUrls.length > 0 && (
-          <div className="space-y-3">
-            {videoUrls.map((url, idx) => {
-              const youtubeId = getYouTubeId(url);
-              const vimeoId = getVimeoId(url);
-
-              if (youtubeId) {
-                return <YouTubeEmbed key={idx} videoId={youtubeId} url={url} />;
-              }
-
-              if (vimeoId) {
-                return (
-                  <div key={idx} className="w-full rounded-xl overflow-hidden border border-white/20 shadow-lg">
-                    <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0 }}>
-                      <iframe
-                        style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-                        src={`https://player.vimeo.com/video/${vimeoId}`}
-                        frameBorder="0"
-                        allow="autoplay; fullscreen; picture-in-picture"
-                        allowFullScreen
-                      />
-                    </div>
-                  </div>
-                );
-              }
-
-              return null;
-            })}
-          </div>
-        )}
+        <VideoEmbeds videoUrls={videoUrls} />
 
 
 
